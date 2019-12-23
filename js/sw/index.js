65f600e4163d97ed2e380365bb9716cf1b8f505a
Mohamed Hassan <m.h.h.305405@gmail.com>
2019-12-23 16:45:23 +0200

    fix: Service worker

--- js/sw/index.js
+++ js/sw/index.js
@@ -1,22 +1,52 @@
-self.addEventListener('install', function(event) {
+let staticCacheName = 'restaurant-app-v1';
+
+let urlsToCache = [
+    '/',
+    '/index.html',
+    '/retaurant.html',
+    '/css/styles.css',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+    '/data/restaurant.json',
+    '/img',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('restaurant-app-v1').then((cache) => {
-            cache.addAll([
-                '/',
-                '/css/styles.css',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/js/dbhelper.js',
-                '/data/restaurant.json',
-                '/img'
-            ]);
+        caches.open(staticCacheName).then((cache) => {
+            cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== staticCacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request, {ignoreSearch: true}).then((response) => {
             return response || fetch(event.request);
         })
     );
